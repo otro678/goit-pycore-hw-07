a69ed61df854c666791cdbfbbd18addb504f0fe5
Serhii K <serhii.kozachenko.92@gmail.com>
2024-07-29 23:24:14 +0300

    Added bday that is this week for ex01

--- ex01.py
+++ ex01.py
@@ -14,10 +14,18 @@ jane_record.add_phone("9876543210")
 jane_record.add_birthday("05.08.1985")
 book.add_record(jane_record)
 
+jack_record = Record("Jack")
+jack_record.add_phone("9996543210")
+jack_record.add_birthday("03.08.1985")
+book.add_record(jack_record)
+
 def show_birthdays(shift_congrats_to_monday):
     upcoming_birthdays = book.get_upcoming_birthdays(shift_congrats_to_monday)
     for birthday_info in upcoming_birthdays:
         print(f"Upcoming birthday: {birthday_info['name']} on {birthday_info['birthday']}, congratulation date: {birthday_info['congratulation_date']}")
 
+print("Congratulations on Friday if bday's on a weekend:")
 show_birthdays(False)
+
+print("Congratulations next Monday if bday's on a weekend:")
 show_birthdays(True)
